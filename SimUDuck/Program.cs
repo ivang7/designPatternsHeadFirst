7305127fcb164ed0a51ec915ebf7aa3f03d73f80
Иван <ivan.g@inbox.ru>
2019-10-03 00:02:12 +0300

    add new fly behavior and change in runtime

--- SimUDuck/Program.cs
+++ SimUDuck/Program.cs
@@ -1,4 +1,5 @@
-﻿using SimUDuckApp.Ducks;
+﻿using SimUDuckApp.Behaviors;
+using SimUDuckApp.Ducks;
 using System;
 using System.Collections.Generic;
 
@@ -9,12 +10,27 @@ namespace SimUDuckApp
         static void Main(string[] args)
         {
             var ducks = new List<Duck>();
-            
+
             ducks.Add(new MallardDuck());
             ducks.Add(new RedheadDuck());
             ducks.Add(new DecoyDuck());
             ducks.Add(new MallardDuck());
             ducks.Add(new RubberDuck());
+
+            foreach (var duck in ducks)
+            {
+                duck.Display();
+                duck.Quack();
+                duck.Swim();
+                duck.Quack();
+                duck.Fly();
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Add rocket for all ducks");
+            Console.WriteLine("===================================");
+            ducks.ForEach(d => d.FlyBehavior = new FlyRocketPowered());
 
             foreach (var duck in ducks)
             {
